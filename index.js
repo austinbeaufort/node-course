2f7f2dc5ef1d126c0255aca68f749db21fa9cf0c
Austin Beaufort <tinyhatcoding@gmail.com>
2019-06-09 09:47:38 -0400

    Learning Node JS in direct parallel with a python course to develop a deep understanding

---
+++ index.js
@@ -0,0 +1,14 @@
+// Accept User Input via the Terminal.
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question('What is your name? ', name => {
+    console.log(`Hi ${name}`);
+    rl.close();
+});
+
